7ecc5517f401db90b33fdef6c5246355a7fb1996
ctrl0703 <ctrl0703@ctrl0703s-MacBook-Pro.local>
2018-08-26 22:53:49 +0900

    conflict error

--- src/main/java/io/younhamle/memorycard/entities/SmartCard.java
+++ src/main/java/io/younhamle/memorycard/entities/SmartCard.java
@@ -11,21 +11,25 @@ public class SmartCard {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
+	
 	private String title;
 	
 	public SmartCard() {
 		
 	}
 	
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
-	public void setId(int id) {
+
+	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+	
 	public void setTitle(String title) {
 		this.title = title;
 	}
