29f082b296e00f03a79de2d41d4f30cdfcb44106
ctrl0703 <ctrl0703@ctrl0703s-MacBook-Pro.local>
2018-08-28 20:22:02 +0900

    allow option added

--- src/main/java/io/younhamle/memorycard/controller/SmartMemoryCardController.java
+++ src/main/java/io/younhamle/memorycard/controller/SmartMemoryCardController.java
@@ -3,6 +3,7 @@ package io.younhamle.memorycard.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ public class SmartMemoryCardController {
 	@Autowired
 	private SmartCardService smartCardService;
 	
+	@CrossOrigin(origins = "*")
 	@GetMapping(value="/smartcards")
 	public List<SmartCard> smartcards() {
 		return smartCardService.getAllSmartCards();
